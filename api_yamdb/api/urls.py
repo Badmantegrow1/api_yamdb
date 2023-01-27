7c3733301170535a6cf4f5a6ee18e109d24c2131
Konstantin S <37315051+Badmantegrow1@users.noreply.github.com>
2023-01-27 03:32:39 +0300

    дописал немного код views, urls на токен и регистрацию

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,23 +2,25 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import (CategoryViewSet, GenreViewSet, CommentViewSet,
-                       ReviewViewSet, TitleViewSet, UsersViewSet)
+                       ReviewViewSet, TitleViewSet, UsersViewSet,
+                       get_jwt_token, register)
 
 router = DefaultRouter()
 
-router.register('categories', CategoryViewSet)
-router.register('genres', GenreViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'genres', GenreViewSet)
 router.register(
-    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet
+    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews'
 )
-router.register('titles', TitleViewSet)
-router.register('users', UsersViewSet)
+router.register(r'titles', TitleViewSet)
+router.register(r'users', UsersViewSet)
 router.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentViewSet
-)
+    CommentViewSet, basename='comments')
 
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('v1/auth/signup/', register, name='register'),
+    path('v1/auth/token/', get_jwt_token, name='token')
 ]
