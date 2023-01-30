41acde3dc6a9d7783bbf870f2e9523cec755377d
Konstantin S <37315051+Badmantegrow1@users.noreply.github.com>
2023-01-31 02:47:54 +0300

    правка urls 2

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import (CategoryViewSet, GenreViewSet, CommentViewSet,
-                       ReviewViewSet, TitleViewSet, UsersViewSet,
+                       ReviewViewSet, TitleViewSet, UserViewSet,
                        get_jwt_token, register)
 
 router = DefaultRouter()
