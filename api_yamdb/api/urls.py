a36b961f317fe99785a095aa7c3d04e502aaa5c1
Konstantin S <37315051+Badmantegrow1@users.noreply.github.com>
2023-01-31 02:48:43 +0300

    правка urls 3

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -13,7 +13,7 @@ router.register(
     r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews'
 )
 router.register(r'titles', TitleViewSet)
-router.register(r'users', UsersViewSet)
+router.register(r'users', UserViewSet)
 router.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet, basename='comments')
