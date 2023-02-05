194595a1e214869269fb8dd4ecbac0d470c8e021
schizgod <schizgod@gmail.com>
2023-02-05 20:52:43 +0300

    Правка методов во views, isort импортов

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -6,7 +6,7 @@ from django.core.mail import send_mail
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status, viewsets
+from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -97,7 +97,10 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     permission_classes = (IsAdminUser,)
     serializer_class = UserSerializer
+    filter_backends = (filters.SearchFilter,)
     lookup_field = 'username'
+    search_fields = ('username',)
+    http_method_names = ['get', 'post', 'patch', 'delete']
 
     @action(
         detail=False,
@@ -144,7 +147,6 @@ def create_user(request):
             data={'error': 'Ошибка при отправки кода подтверждения!'},
             status=status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
-
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,54 +1,61 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from users.validators import UsernameValidator
+
+USER = 'user'
+MODERATOR = 'moderator'
+ADMIN = 'admin'
 
 
 class User(AbstractUser):
-    """Модель создания пользователя."""
 
-    USER = 'user'
-    MODERATOR = 'moderator'
-    ADMIN = 'admin'
-
-    USER_ROLES = [
-        (USER, 'user'),
-        (MODERATOR, 'moderator'),
-        (ADMIN, 'admin'),
-    ]
-    email = models.EmailField(
-        max_length=254,
+    roles = (
+        (USER, USER),
+        (MODERATOR, MODERATOR),
+        (ADMIN, ADMIN),
+    )
+    username_validator = UsernameValidator()
+    username = models.CharField(
+        'Имя пользователя',
+        max_length=150,
         unique=True,
+        validators=[username_validator],
     )
-    bio = models.TextField(
-        verbose_name='Биография',
-        blank=True,
-    )
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField('Email', max_length=254, unique=True)
     role = models.CharField(
-        verbose_name='Роль пользователя',
-        max_length=10,
-        choices=USER_ROLES,
-        default='user',
+        'Роль пользователя',
+        choices=roles,
+        max_length=max(len(role[1]) for role in roles), default=USER
     )
+    bio = models.TextField('Биография', blank=True)
     confirmation_code = models.CharField(
-        verbose_name='Токен пользователя',
+        'Код подтверждения',
         max_length=100,
-        blank=True,
-        null=True,
+        null=True
     )
+
+    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELDS = 'email'
+
+    def __str__(self):
+        return str(self.username)
 
     @property
     def is_admin(self):
         """Проверка пользователя на наличие прав администратора."""
-        return self.role == self.ADMIN or self.is_superuser
+        return self.role == "admin" or self.is_superuser
 
     @property
     def is_moderator(self):
         """Проверка пользователя на наличие прав модератора."""
-        return self.role == self.MODERATOR
+        return self.role == "moderator"
 
     @property
     def is_user(self):
         """Проверка пользователя на наличие стандартных прав."""
-        return self.role == self.USER
+        return self.role == "user"
 
     class Meta:
         ordering = ('username',)
